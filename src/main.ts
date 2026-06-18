2b23c1d1cfb0fdd0d3edc02e939ad3f7b3dc245c
Alex <steblyakov.aleksandr@gmail.com>
2026-06-18 23:40:03 +0300

    Lesson 15 - switching to render to fix latency issues - fixing some project settings

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import { AppModule } from './app.module';
 import { appSetup } from './setup/app.setup';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  appSetup(app); //глобальные настройки приложения
+    appSetup(app); //глобальные настройки приложения
 
-  const PORT = process.env.PORT || 5005; //TODO: move to configService. will be in the following lessons
+    const PORT = process.env.PORT || 5005; //TODO: move to configService. will be in the following lessons
 
-  await app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT);
-  });
+    await app.listen(process.env.PORT || 5005, () => {
+        console.log('Server is running on port ' + PORT);
+    });
 }
 
 bootstrap();
